5c5126b8c4d3b1abf7cd12926b4f3f0ee284b976
andrevale99 <andrefvale@gmail.com>
2026-03-21 09:51:32 -0300

    Criação dos arquivos para o projeto do controlador por espaço de estados.

---
+++ StateSpaceControl/components/drv8833/drv8833.c
@@ -0,0 +1,9 @@
+#include "drv8833.h"
+
+static const char *TAG = "DRV8833";
+
+esp_err_t drv8833_init(drv8833_config_t *config)
+{
+
+    return ESP_OK;    
+}
---
+++ StateSpaceControl/main/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "drv8833.h"
+
+void app_main(void)
+{
+
+}
