93cdcdd5ca87729a3e2d3a22e573dc1c879f5b79
andrevale99 <andrefvale@gmail.com>
2026-03-26 20:32:03 -0300

    Inicialização dos pinos

--- ControleSS_MotorDC/src/main.c
+++ ControleSS_MotorDC/src/main.c
@@ -4,19 +4,28 @@
 
 int main(void)
 {
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIOBEN;
     // Pinos A8 (TIM1_CH1) e A9 (TIM1_CH2)
     drv8833_config_t motorA = {
         .IN[0].gpioReg = GPIOA,
         .IN[0].in = 8,
         .IN[0].AlternateFuncSel = GPIO_AFRH_AFSEL8_0,
         .IN[0].OutputMode = (0x2 << GPIO_MODER_MODE8_Pos),
-    
+
         .IN[1].in = DRV8833_DISCONNECT_GPIO,
         .IN[2].in = DRV8833_DISCONNECT_GPIO,
         .IN[3].in = DRV8833_DISCONNECT_GPIO,
+
+        .SLEEP.gpioReg = GPIOB,
+        .SLEEP.in = 12,
     };
 
     drv8833_init(&motorA);
+    drv8833_set_sleep_state(&motorA, 1);
+
+    while (1)
+    {
+    }
 
     return 0;
 }
