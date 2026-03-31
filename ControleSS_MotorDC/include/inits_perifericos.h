75857f6da0629ddfe0fd54d1fbe28abb2c0c51e9
andrevale99 <andrefvale@gmail.com>
2026-03-31 20:18:32 -0300

    Organização

--- ControleSS_MotorDC/include/inits_perifericos.h
+++ ControleSS_MotorDC/include/inits_perifericos.h
@@ -3,40 +3,6 @@
 
 #include <stm32f411xe.h>
 
-#define WRITE 1
-#define CMD 0
-
-#define CLEAR_DISPLAY 0x01
-#define RETURN_HOME 0x02
-#define SECOND_LINE 0xC0
-
-#define ENTRY_MODE_SET 0x04
-#define INCREMENT (ENTRY_MODE_SET | 0x02)     // I
-#define DECREMENT (ENTRY_MODE_SET | 0x00)     // D
-#define SHIFT_DISPLAY (ENTRY_MODE_SET | 0x01) // S
-
-#define DISPLAY_CONTROL 0x08
-#define DISPLAY_OFF (DISPLAY_CONTROL | 0x00)  // D
-#define DISPLAY_ON (DISPLAY_CONTROL | 0x04)   // D
-#define CURSOR_ON (DISPLAY_CONTROL | 0x02)    // C
-#define BLINK_CURSOR (DISPLAY_CONTROL | 0x01) // B
-
-#define CURSOR_SHIFT 0x10
-#define DISPLAY_SHIFT (CURSOR_SHIFT | 0x08) // S
-#define CURSOR_MOVE (CURSOR_SHIFT | 0x00)   // C
-#define SHIFT_RIGHT (CURSOR_SHIFT | 0x04)   // R
-#define SHIFT_LEFT (CURSOR_SHIFT | 0x00)    // L
-
-#define FUNCTION_SET 0x20
-#define BITS_8 (FUNCTION_SET | 0x00)    // DL
-#define BITS_4 (FUNCTION_SET | 0x01)    // DL
-#define LINES_1 (FUNCTION_SET | 0x00)   // N
-#define LINES_2 (FUNCTION_SET | 0x08)   // N
-#define FONT_5x8 (FUNCTION_SET | 0x00)  // F
-#define FONT_5x10 (FUNCTION_SET | 0x04) // F
-
-#define SET_DDRAM 0x80
-
 void rcc_enable_clocks(void)
 {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
@@ -180,9 +146,9 @@ void lcd16x2_cmd(uint8_t cmd, uint8_t rs_state)
     GPIOA->BSRR = (0xF << 16);
     GPIOA->BSRR = ((cmd >> 4) & 0xF); /* sending upper nibble */
 
-    if (rs_state == CMD)
+    if (rs_state == 0) // command ?
         GPIOB->BSRR = (1 << 29); /* RS=0, command reg. */
-    else
+    else // write ?
         GPIOB->BSRR = (1 << 13); /* RS=1, command reg. */
 
     lcd16x2_pulse_enable();
@@ -191,7 +157,7 @@ void lcd16x2_cmd(uint8_t cmd, uint8_t rs_state)
     GPIOA->BSRR |= (cmd & 0xF); /* sending lower nibble */
     lcd16x2_pulse_enable();
 
-    if (rs_state == WRITE)
+    if (rs_state == 1) // write ?
         GPIOB->BSRR |= (1 << 29); /* RS=0, command reg. */
 
     delay_ms(50);
