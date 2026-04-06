5b2f7abec8c58588023dc3fb38e6dcbabac8d8ab
andrevale99 <andrefvale@gmail.com>
2026-04-06 20:21:43 -0300

    Função para escrever no lcd

--- ControleSS_MotorDC/src/main.c
+++ ControleSS_MotorDC/src/main.c
@@ -10,33 +10,29 @@
 #include "lcd16x2.h"
 
 volatile int32_t cnt = 0;
+char buffer[18];
 
-void TIM4_IRQHandler(void)
-{
-    if (TIM4->SR & TIM_SR_UIF)
-    {
-        cnt = (int16_t)TIM3->CNT;
-        TIM3->CNT = 0;
-        TIM4->SR &= ~TIM_SR_UIF;
-        // Estouro aproximadamente em 49ms
-        // Limpar bit de evento
-    }
-}
+void write_interface(lcd16x2_handle *lcd);
+void write_data(lcd16x2_handle *lcd);
+void TIM4_IRQHandler(void);
 
 int main(void)
 {
     setup_system();
 
     motor_dc_handle_t motor = {
-        .m1.in[0].set_state = gpio_drv8833_set_state_in1,
-        .m1.pwm.set_duty_cycle = tim1_channel1_pwm_set_duty,
-        .m1.pwm.maxDuty = 999,
-        .m1.sleep = gpio_dr8833_set_state_sleep,
+        .m1 = {
+            .in[0].set_state = gpio_drv8833_set_state_in1,
+            .pwm.set_duty_cycle = tim1_channel1_pwm_set_duty,
+            .pwm.maxDuty = 999,
+            .sleep = gpio_dr8833_set_state_sleep},
 
-        .m2.in[1].set_state = gpio_drv8833_set_state_in2,
-        .m2.pwm.set_duty_cycle = tim1_channel2_pwm_set_duty,
-        .m2.pwm.maxDuty = 999,
-        .m2.sleep = gpio_dr8833_set_state_sleep,
+        .m2 = {
+            .in[1].set_state = gpio_drv8833_set_state_in2,
+            .pwm.set_duty_cycle = tim1_channel2_pwm_set_duty,
+            .pwm.maxDuty = 999,
+            .sleep = gpio_dr8833_set_state_sleep,
+        },
     };
 
     lcd16x2_handle lcd = {
@@ -53,20 +49,56 @@ int main(void)
 
     lcd16x2_init_4bits(&lcd);
 
-    motor_dc_pwm_channel(&motor, 0, 999);
+    motor_dc_pwm_channel(&motor, 1, 999);
 
-    char buffer[8] = {0};
+    write_interface(&lcd);
+
     while (1)
     {
-        snprintf(buffer, 8, "%li", cnt);
-
-        for (char *i = buffer; *i != '\0'; i++)
-            lcd16x2_send_data(&lcd, *i);
-
-        delay_ms(1000);
-        lcd16x2_send_cmd(&lcd, RETURN_HOME);
-        lcd16x2_send_cmd(&lcd, CLEAR_DISPLAY);
+        write_data(&lcd);
+        delay_ms(1000);  
     }
-
+    //204 pulsos em 50 ms *165 rpm
     return 0;
+}
+
+void write_interface(lcd16x2_handle *lcd)
+{
+    uint8_t size = 0;
+
+    size = sprintf(buffer, "Pulsos:", cnt);
+    lcd16x2_write_string(lcd, buffer, size);
+
+    lcd16x2_send_cmd(lcd, SECOND_LINE);
+    size = sprintf(buffer, "RPM:", cnt);
+    lcd16x2_write_string(lcd, buffer, size);
+
+    lcd16x2_send_cmd(lcd, RETURN_HOME);
+}
+
+void write_data(lcd16x2_handle *lcd)
+{
+    uint8_t size = 0;
+
+    size = sprintf(buffer, "%li ", cnt);
+    lcd16x2_send_cmd(lcd, SET_DDRAM | 0x08);
+    lcd16x2_write_string(lcd, buffer, size);
+
+    lcd16x2_send_cmd(lcd, SECOND_LINE | 0x5);
+    size = sprintf(buffer, "%li ", cnt);
+    lcd16x2_write_string(lcd, buffer, size);
+
+    lcd16x2_send_cmd(lcd, RETURN_HOME);
+}
+
+void TIM4_IRQHandler(void)
+{
+    if (TIM4->SR & TIM_SR_UIF)
+    {
+        cnt = (int16_t)TIM3->CNT;
+        TIM3->CNT = 0;
+        TIM4->SR &= ~TIM_SR_UIF;
+        // Estouro aproximadamente em 49ms
+        // Limpar bit de evento
+    }
 }
