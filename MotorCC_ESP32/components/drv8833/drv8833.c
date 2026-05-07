aaeeb5c989fe6fa39992e599e78575eb1066ffb8
andrevale99 <andrefvale@gmail.com>
2026-05-07 08:48:29 -0300

    DRV8833 componente

--- MotorCC_ESP32/components/drv8833/drv8833.c
+++ MotorCC_ESP32/components/drv8833/drv8833.c
@@ -1,7 +1,27 @@
-#include <stdio.h>
 #include "drv8833.h"
 
-void func(void)
+esp_err_t drv8833_init(drv8833_t *drv)
 {
 
+    return ESP_OK;
+}
+
+esp_err_t drv8833_set_in_level(drv8833_t *drv, uint8_t level)
+{
+    return ESP_OK;
+}
+
+esp_err_t drv8833_set_in_pwm(drv8833_t *drv, uint32_t duty)
+{
+    return ESP_OK;
+}
+
+esp_err_t drv8833_set_sleep_level(drv8833_t *drv, uint8_t level)
+{
+    return ESP_OK;
+}
+
+int drv8833_get_fault_situation(drv8833_t *drv)
+{
+    return -1;
 }
--- MotorCC_ESP32/components/drv8833/include/drv8833.h
+++ MotorCC_ESP32/components/drv8833/include/drv8833.h
@@ -1,1 +1,34 @@
-void func(void);
+#ifndef DRV8833_H
+#define DRV8833_H
+
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+
+#include "esp_err.h"
+#include "esp_log.h"
+
+#define DRV8833_NUM_CHANNELS 4
+
+
+typedef struct
+{
+    gpio_config_t in[DRV8833_NUM_CHANNELS];
+    gpio_config_t sleep;
+    gpio_config_t fault;
+    ledc_timer_config_t PwmTimer;
+
+}drv8833_t;
+
+
+esp_err_t drv8833_init(drv8833_t *drv);
+
+esp_err_t drv8833_set_in_level(drv8833_t *drv, uint8_t level);
+
+esp_err_t drv8833_set_in_pwm(drv8833_t *drv, uint32_t duty);
+
+esp_err_t drv8833_set_sleep_level(drv8833_t *drv, uint8_t level);
+
+int drv8833_get_fault_situation(drv8833_t *drv);
+
+
+#endif
