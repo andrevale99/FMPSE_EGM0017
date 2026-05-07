304fbadc9e4884a61103d2749a2fbf39dd182d53
andrevale99 <andrefvale@gmail.com>
2026-05-07 17:14:59 -0300

    Criação das filas

---
+++ MotorCC_ESP32/components/system/include/sys_queues.h
@@ -0,0 +1,11 @@
+#ifndef SYS_QUEUES_H
+#define SYS_QUEUES_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+QueueHandle_t queue_isr_motor_task = NULL;
+
+
+#endif
