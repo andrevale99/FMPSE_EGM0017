388f7b0d44f2f114561c9b4d6dc8393b4c4db442
andrevale99 <andrefvale@gmail.com>
2026-05-08 09:10:17 -0300

    update

--- MotorCC_ESP32/main/main.c
+++ MotorCC_ESP32/main/main.c
@@ -1,20 +1,8 @@
 #include <stdio.h>
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-#include "esp_log.h"
-#include "esp_err.h"
-
 #include "system.h"
-
 
 void app_main(void)
 {
     system_init();
-
-    while(1)
-    {
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
 }
